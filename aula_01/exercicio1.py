057c1f67ab66cb76c1226244150419c379f4316b
Deyvidjh30 <c.deyvid@escolar.ifrn.edu.br>
2025-04-10 14:11:12 +0000

    Alterações + atividade 1

--- aula_01/exercicio1.py
+++ aula_01/exercicio1.py
@@ -1,1 +1,14 @@
-print("aa")
+
+# atividade 1
+# print("digite seu nome:")
+# seu_nome=input()
+# print("digite sua matricula:")
+# sua_matricula=input()
+# print(f"seu nome é: {seu_nome}")
+# print(f"sua matricula é: {sua_matricula}")
+
+
+# atividade 1.2
+print("digite seu primeiro nome:")
+seu_nome=input()
+print(f"Bem-vindo(a) ao Codespaces, {seu_nome}")
