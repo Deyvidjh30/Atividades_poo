b16a0197bc164940c2d4fd8225024f2fd48a793e
Deyvidjh30 <c.deyvid@escolar.ifrn.edu.br>
2025-04-15 13:48:56 +0000

    primeiras atividades

--- aula_01/exercicio1.py
+++ aula_01/exercicio1.py
@@ -1,4 +1,4 @@
-
+"""lista_1"""
 # atividade 1
 # print("digite seu nome:")
 # seu_nome=input()
@@ -8,7 +8,26 @@
 # print(f"sua matricula é: {sua_matricula}")
 
 
-# atividade 1.2
-print("digite seu primeiro nome:")
-seu_nome=input()
-print(f"Bem-vindo(a) ao Codespaces, {seu_nome}")
+# # atividade 1.2
+# print("digite seu primeiro nome:")
+# seu_nome=input()
+# print(f"Bem-vindo(a) ao Codespaces, {seu_nome}")
+""""________________________________________"""
+
+"""lista_2"""
+# atividade 1
+# print("digite seu nome completo:")
+# nome_completo=str(input())
+# primeiro_nome=nome_completo.split()[0]
+# print(f"Bem-vindo(a) ao Python, {primeiro_nome}")
+
+# atividade 2
+print("Digite a nota do primeiro bimestre da disciplina:")
+nota_peso2=int(input())
+print("Digite a nota do segundo bimestre da disciplina:")
+nota_peso3=int(input())
+media_parcial_soma=nota_peso2*2 + nota_peso3*3
+media_parcial=media_parcial_soma/5
+print(f"Média parcial: {media_parcial}")
+
+
