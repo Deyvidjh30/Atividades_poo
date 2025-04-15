ccc8b7c2685fbc56fda0ee8d53a3e74afe2140a5
Deyvidjh30 <c.deyvid@escolar.ifrn.edu.br>
2025-04-15 14:14:11 +0000

    listas 1 e 2

---
+++ aula_01/listas.py
@@ -0,0 +1,49 @@
+import math
+"""lista_1"""
+# atividade 1
+# print("digite seu nome:")
+# seu_nome=input()
+# print("digite sua matricula:")
+# sua_matricula=input()
+# print(f"seu nome é: {seu_nome}")
+# print(f"sua matricula é: {sua_matricula}")
+
+
+# # atividade 2
+# print("digite seu primeiro nome:")
+# seu_nome=input()
+# print(f"Bem-vindo(a) ao Codespaces, {seu_nome}")
+""""________________________________________"""
+
+"""lista_2"""
+# atividade 1
+# print("digite seu nome completo:")
+# nome_completo=str(input())
+# primeiro_nome=nome_completo.split()[0]
+# print(f"Bem-vindo(a) ao Python, {primeiro_nome}")
+
+# atividade 2
+# print("Digite a nota do primeiro bimestre da disciplina:")
+# nota_peso2=int(input())
+# print("Digite a nota do segundo bimestre da disciplina:")
+# nota_peso3=int(input())
+# media_parcial_soma=nota_peso2*2 + nota_peso3*3
+# media_parcial=media_parcial_soma/5
+# print(f"Média parcial: {media_parcial}")
+
+
+#atividade 3
+# base = float(input("Digite a base do retângulo: "))
+# altura = float(input("Digite a altura do retângulo: "))
+# area = base * altura
+# perimetro = 2 * (base + altura)
+# diagonal = math.sqrt(base**2 + altura**2)
+# print(f"Área = {area:.2f} - Perímetro = {perimetro:.2f} - Diagonal = {diagonal:.2f}")
+
+#atividade 4
+# print("digite uma frase e irei mostrar a ultima palavra")
+# frase_completa=str(input())
+# ultima_palavra=frase_completa.split()[-1]
+# print(f"Sua ultima palavra foi: {ultima_palavra}")
+
+""""________________________________________"""
