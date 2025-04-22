ec305e3424510fe587adaf2bcfce1c5e8e097cfe
Deyvidjh30 <c.deyvid@escolar.ifrn.edu.br>
2025-04-22 13:30:02 +0000

    listas novas

---
+++ Listas/lista_1.py
@@ -0,0 +1,17 @@
+import math
+
+"""lista_1"""
+# atividade 1
+# print("digite seu nome:")
+# seu_nome=input()
+# print("digite sua matricula:")
+# sua_matricula=input()
+# print(f"seu nome é: {seu_nome}")
+# print(f"sua matricula é: {sua_matricula}")
+
+
+# # atividade 2
+# print("digite seu primeiro nome:")
+# seu_nome=input()
+# print(f"Bem-vindo(a) ao Codespaces, {seu_nome}")
+""""________________________________________"""
---
+++ lista_5.py
@@ -0,0 +1,35 @@
+#atividade 1
+"""def maior(x, y):
+    m=(max(x, y))
+    return m
+
+x=int(input())
+y=int(input())
+print(maior(x, y))"""
+
+#atividade 2
+"""def maior(x, y, z):
+    m=(max(x, y, z))
+    return m
+
+x=int(input())
+y=int(input())
+z=int(input())
+print(maior(x, y, z))"""
+
+#atividade 3
+"""def iniciais(nome):
+    iniciais = []
+    for i in range(len(nome)):
+        nome[i] = list(nome[i])
+        iniciais.append(nome[i][0])
+    return iniciais
+
+nome = input().split()
+inicial = iniciais(nome)
+print(inicial)"""
+
+#atividade 4
+
+
+
