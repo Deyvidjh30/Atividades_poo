5401936f3e5b009221c9210eb064fe4213c0a707
Deyvidjh30 <c.deyvid@escolar.ifrn.edu.br>
2025-05-12 13:48:57 +0000

    aaaaaaaaaaa

---
+++ atividade.py
@@ -0,0 +1,54 @@
+# # questão 1:
+
+# I: 11
+# II: -10
+# III: 14
+# IV: 4
+# V: 2.5
+
+# # questão 2:
+
+# I:
+
+# a, b, c = map(int, input().split())
+
+# II:
+
+# resultado = 0
+# for i in [a, b, c]:
+# 	if i % 2 == 0:
+# 		resultado += i
+
+# print(resultado)
+
+# questão 3:
+
+# texto = input()
+
+# resultado = ""
+
+# for i in range(len(texto)):
+#  	if i % 2 == 0:
+#         	resultado += texto[i]
+
+# print(resultado)
+
+
+# questão 4:
+class Agua():
+	def _init_(self):
+		self.mes = 0
+		self.ano = 0
+		self.consumo = 0
+
+	def calc_consumo(self):
+		self.consumo = input()
+		if self.consumo <= 10:
+			self.custo = 38
+		elif self.consumo > 10 and self.consumo <= 20:
+			for i in range(self.consumo-10):
+				self.custo += 5
+			self.custo += 38
+
+
+
